4b94dc8552c8c02af3d1edfb1b75eb97b436732b
Rishav Mehra <rishavmehra61@gmail.com>
2024-10-08 21:43:34 +0530

    aws preSigned image Url, jwt setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,15 @@
 import express from "express";
+import userRouter from "./routers/user";
+import workerRouter from "./routers/worker";
 
 const app = express(); 
 
+app.use(express.json())
+
+export const JWT_SECRET = "rishav"
+app.use("/v1/user", userRouter);
+app.use("/v1/worker", workerRouter);
+
+
+
+app.listen(3000)
